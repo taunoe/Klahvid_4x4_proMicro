c1d597ad1be183b2ad589315c279d5434a1ffc7e
Tauno Erik <sedumacre@gmail.com>
2023-04-05 21:01:05 +0300

    leds are working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 /**
  *  Project: Klahvid_4x4_proMicro
  *  Started: 26.04.2021
- *  Edited:  08.05.2021
+ *  Edited:  05.04.2023
  * 
- *  Copyright 2021 Tauno Erik
+ *  Copyright 2023 Tauno Erik
  * 
  *  Links:
  *  - https://learn.sparkfun.com/tutorials/cherry-mx-switch-breakout-hookup-guide
@@ -12,47 +12,115 @@
 #include <Arduino.h>
 #include "Keypad.h"
 
-const int SER_PIN = 10;    // Pin for the SER (serial data input) of the shift register
-const int SRCLK_PIN = 11;  // Pin for the SRCLK (shift register clock input) of the shift register
-const int RCLK_PIN = 12;   // Pin for the RCLK (storage register clock input) of the shift register
+const uint RANDOM_SEED_PIN = 0;
 
+// Shift register pins
+const uint SER_PIN = 10;    // SER - serial data input
+const uint SRCLK_PIN = 11;  // SRCLK - shift register clock input
+const uint RCLK_PIN = 12;   // RCLK - storage register clock input
 
-const int ROWS = 4; // Number of rows on the keypad
-const int COLS = 4; // Number of columns on the keypad
+uint old_random_num;
+uint new_random_num;
 
-// Define the keymap
-char keys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+// Keypad
+const uint ROWS_NUM = 4;
+const uint COLS_NUM = 4;
+
+// Keymap
+char keys[ROWS_NUM][COLS_NUM] = {
+  {'4','8','b','f'},
+  {'3','7','a','e'},
+  {'2','6','0','d'},
+  {'1','5','9','c'}
 };
 
-// Define the row and column pins on the Arduino
-byte rowPins[ROWS] = { 2, 3, 4, 5 };
-byte colPins[COLS] = { 6, 7, 8, 9 };
+// Markings on board
+const uint8_t C1_PIN = 2;
+const uint8_t C2_PIN = 3;
+const uint8_t C3_PIN = 4;
+const uint8_t C4_PIN = 5;
+const uint8_t R1_PIN = 6;
+const uint8_t R2_PIN = 7;
+const uint8_t R3_PIN = 8;
+const uint8_t R4_PIN = 9;
 
-// Create the keypad object
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+// Row and column pins
+byte ROW_PINS[ROWS_NUM] = {C1_PIN, C2_PIN, C3_PIN, C4_PIN};
+byte COL_PINS[COLS_NUM] = {R1_PIN, R2_PIN, R3_PIN, R4_PIN};
+
+// Keypad object
+Keypad keypad = Keypad(makeKeymap(keys), ROW_PINS, COL_PINS, ROWS_NUM, COLS_NUM);
+
+// Check if the number is even
+bool is_even(uint num) {
+  if (num % 2 == 0) {
+    //Serial.println(num + " is even.");
+    return true;
+  } else { 
+    //Serial.println(num + " is odd.");
+    return false;
+  }
+}
+
+void all_leds_off() {
+  for (uint i = 0; i < 2; i++) {
+    shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, 0b000000);
+  }
+  digitalWrite(RCLK_PIN, HIGH);
+  digitalWrite(RCLK_PIN, LOW);
+}
 
 void setup() {
-  Serial.begin(9600); // Start serial communication
+  Serial.begin(9600);
 
   pinMode(SER_PIN, OUTPUT);
   pinMode(SRCLK_PIN, OUTPUT);
   pinMode(RCLK_PIN, OUTPUT);
+
+  // if analog input pin is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(RANDOM_SEED_PIN));
+  all_leds_off();
 }
 
 void loop() {
   char key = keypad.getKey(); // Read the keypad
 
-  if (key != NO_KEY) { // If a key is pressed
-    Serial.println(key); // Print the key to the serial monitor
+  if (key) {
+    Serial.print("Key:");
+    Serial.println(key);
+
+    // Lightup new key
+    uint register_a = 0;
+    uint register_b = 0;
+
+    do {
+      new_random_num = random(0, 15);
+    } while (new_random_num == old_random_num);
+
+    Serial.print("Rand:");
+    Serial.println(new_random_num);
+    old_random_num = new_random_num;
+
+    if (new_random_num < 8) {
+      register_a = 1<<new_random_num;
+    } else {
+      register_b = 1<<(new_random_num-8);
+    }
+
+    shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, register_a);
+    shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, register_b);
+    digitalWrite(RCLK_PIN, HIGH);
+    digitalWrite(RCLK_PIN, LOW);
   }
 
-  shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, 0b11111111); // Shift out a byte to the shift register
-  shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, 0b11111111);
+
+/*
+  shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, 0b00000000);
+  shiftOut(SER_PIN, SRCLK_PIN, MSBFIRST, 0b00000000);
   digitalWrite(RCLK_PIN, HIGH); // Latch the output to the shift register
   digitalWrite(RCLK_PIN, LOW);
-  delay(1000);
+*/
 }
